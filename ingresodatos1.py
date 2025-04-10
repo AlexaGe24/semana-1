4266e3db7e3971f03fdaead73153f68e6f0a57a1
AlexaGe24 <168457766+AlexaGe24@users.noreply.github.com>
2025-04-10 17:50:40 -0500

    Add files via upload

---
+++ ingresodatos1.py
@@ -0,0 +1,6 @@
+nombre = input("Ingrese nombre del persona: ")
+edad = int(input("Ingrese edad de persona: "))
+sueldo = float(input("Ingrese el sueldo de la persona: "))
+mensajeFinal = "Nombre:%s\nEdad:%d\nSueldo:%.2f\n" % (nombre,
+edad, sueldo)
+print(mensajeFinal)
